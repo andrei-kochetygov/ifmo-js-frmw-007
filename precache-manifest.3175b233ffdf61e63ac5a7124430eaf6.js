1d6a53fd9db1d411ab4ca950da427a2f47e7160e
flatdragon <flatdragon@yandex.ru>
2020-06-30 07:16:53 +0000

    Deploying to gh-pages from  @ aebfa7a2af4d8f9cba4381e12e9a80ffdfaf9b71 🚀

---
+++ precache-manifest.3175b233ffdf61e63ac5a7124430eaf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f9c8175559250aa192dfe0353b1e3c3",
+    "url": "/ifmo-js-frmw-007/index.html"
+  },
+  {
+    "revision": "4e7b4788aebca2871ea8",
+    "url": "/ifmo-js-frmw-007/static/css/main.af048857.chunk.css"
+  },
+  {
+    "revision": "06f2c30ff4d4980cab1d",
+    "url": "/ifmo-js-frmw-007/static/js/2.fdfb91be.chunk.js"
+  },
+  {
+    "revision": "16b20f0d39da2178839d67e1fac1e563",
+    "url": "/ifmo-js-frmw-007/static/js/2.fdfb91be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e7b4788aebca2871ea8",
+    "url": "/ifmo-js-frmw-007/static/js/main.06eb9bc3.chunk.js"
+  },
+  {
+    "revision": "8c151413b120b7eb4eeb",
+    "url": "/ifmo-js-frmw-007/static/js/runtime-main.1a815a52.js"
+  }
+]);
